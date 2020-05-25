efa18b691fe8e7edeccec59c85cb9bf670849039
FenrirD <fenrir4504@gmail.com>
2020-05-25 16:41:15 +0900

    not local

---
+++ precache-manifest.d96480bd815effacbe80b4c997485242.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e934a6ebf14867020a2ef71d241ea840",
+    "url": "/Test01/index.html"
+  },
+  {
+    "revision": "eef4cfb097465a6cdc9b",
+    "url": "/Test01/static/css/2.d7010979.chunk.css"
+  },
+  {
+    "revision": "5aa36409dfeeab0b91e0",
+    "url": "/Test01/static/css/main.19ef28de.chunk.css"
+  },
+  {
+    "revision": "eef4cfb097465a6cdc9b",
+    "url": "/Test01/static/js/2.d957c97d.chunk.js"
+  },
+  {
+    "revision": "5f141f693db8f4a35ae2ebbc39b23dc1",
+    "url": "/Test01/static/js/2.d957c97d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa36409dfeeab0b91e0",
+    "url": "/Test01/static/js/main.328c744e.chunk.js"
+  },
+  {
+    "revision": "c9f5d215e7e873f9c10e",
+    "url": "/Test01/static/js/runtime-main.0e7e8dfd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Test01/precache-manifest.84cfc9dd53eb7b1647bbfb53e1334541.js"
+  "/Test01/precache-manifest.d96480bd815effacbe80b4c997485242.js"
 );
 
 self.addEventListener('message', (event) => {
